560358937572085630d55dd6f6e978e35c7e8182
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2023-03-11 17:01:20 -0800

    updated gameplan comments for SpectogramView and vote components

--- src/components/SpectrogramView/SpectrogramView.tsx
+++ src/components/SpectrogramView/SpectrogramView.tsx
@@ -1,4 +1,4 @@
-// in this file, we assemble the view of two audio files for the user to playback.
+// in this file, we assemble the view of two audio files for the user to playback. Each time this component loads, it provides two, new and unique, randomly selected sounds.
 // 1. We get two Spectogram components.
 import { Spectrogram } from "../Spectrogram/Spectrogram";
 
--- src/pages/vote.tsx
+++ src/pages/vote.tsx
@@ -1,5 +1,7 @@
-import { Button } from "@/components/VoteButton";
-import { Spectrogram } from "@/components/Spectrogram";
+// This component will maintain state that updates when either vote button is pressed. When either button is pressed, the tally for that sound will increment and the spectogram view will refresh.
+
+import { VoteButton } from "@/components/VoteButton";
+import { SpectrogramView } from "@/components/SpectrogramView";
 
 export default function Vote() {
   return "hello";
